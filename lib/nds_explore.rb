58748d26da52ef2b028f0455f742803624e25c95
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-01-21 03:07:42 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ def print_first_directors_movie_titles
   while outer_index < directors_database.length do
     if outer_index == 0
       directors_database[outer_index][:movies].length.times do |i|
-                puts $directors_database[outer_index][:movies][i][:title]
+        puts directors_database[outer_index][:movies][i][:title]
             end
         end
         outer_index += 1
